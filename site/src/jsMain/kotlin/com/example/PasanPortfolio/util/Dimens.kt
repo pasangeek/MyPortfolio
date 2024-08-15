18554844ae8c2b6b4f4db0a1bb8b3d878aaf22cc
pasangeek <pasan.madusanka9@gmail.com>
2024-08-15 21:16:27 +0900

    add distention file

---
+++ site/src/jsMain/kotlin/com/example/PasanPortfolio/util/Dimens.kt
@@ -0,0 +1,9 @@
+package com.example.PasanPortfolio.util
+
+class Dimens {
+    val BORDER_RADIUS = 8
+    val MAX_CARD_WIDTH = 1000
+    val MAX_CARD_HEIGHT = 600
+    val ICON_SIZE = 24
+    val ICON_SIZE_LG = 32
+}
